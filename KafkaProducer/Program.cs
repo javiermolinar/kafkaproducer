ba0a388054784158aecb3a912b0e59b3fa1072d6
Javier Molina <javier.molina@unit4.com>
2018-07-13 14:39:28 +0200

    Added some more features

--- KafkaProducer/Program.cs
+++ KafkaProducer/Program.cs
@@ -1,27 +1,49 @@
 ﻿using Confluent.Kafka;
 using Confluent.Kafka.Serialization;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace kafkaproducer
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Address of the Kafka server: ");
+            string server = Console.ReadLine();
+
+            Console.Write("Topic name: ");
+            string topic = Console.ReadLine();            
 
             var config = new Dictionary<string, object>
             {
-                { "bootstrap.servers", "localhost:9092" }
+                { "bootstrap.servers", server }
             };
 
             using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
-            {
-                var dr = producer.ProduceAsync("test", null, "Nuevo mensaje").Result;                
-                Console.WriteLine($"Delivered '{dr.Value}' to: {dr.TopicPartitionOffset}");
+            {     
+                while(true)
+                {
+                    var dr = producer.ProduceAsync(topic, null, $"New message at {DateTime.Now}").Result;
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Delivered '{dr.Value}' to: {dr.TopicPartitionOffset}");
+                }                
             }
+        }
+
+        private static IConfigurationRoot StartConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            return configuration;
         }
     }
 }
