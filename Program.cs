cd7cdd2f8c965979230468abb2a82801bfb1bb6a
Javier Molina <javier.molina@unit4.com>
2018-07-13 11:21:16 +0200

    Basic producer first version

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kafkaproducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "localhost:9092" }
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                var dr = producer.ProduceAsync("test", null, "test message text").Result;
+                Console.WriteLine($"Delivered '{dr.Value}' to: {dr.TopicPartitionOffset}");
+            }
+        }
+    }
+}
